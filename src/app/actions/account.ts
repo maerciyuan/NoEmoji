ee3ddfbac2853342b3fbf68aa8d2fd9c08ad8254
skjsjhb <skjsjhb@outlook.com>
2025-10-13 13:48:05 +0800

    Add user display page

--- src/app/actions/account.ts
+++ src/app/actions/account.ts
@@ -12,6 +12,7 @@ export async function loginOrRegister(user: string, pwd: string) {
  */
 export async function getUserInfo(): Promise<UserInfo> {
     return {
+        uid: "0",
         name: "root",
         tel: "1234567890A",
         email: "me@example.com",
@@ -19,7 +20,37 @@ export async function getUserInfo(): Promise<UserInfo> {
     };
 }
 
+/**
+ * Called when the browser needs information about all users.
+ */
+export async function getAllUsers(): Promise<UserInfo[]> {
+    return [
+        {
+            uid: "0",
+            name: "root",
+            tel: "1234567890A",
+            email: "me@example.com",
+            super: true
+        },
+        {
+            uid: "1",
+            name: "me",
+            tel: "1234567890A",
+            email: "me@example.com",
+            super: false
+        }
+    ];
+}
+
+/**
+ * Called when the user info needs to be updated.
+ */
+export async function updateUser(ui: UserInfo) {
+
+}
+
 export interface UserInfo {
+    uid: string;
     name: string;
     tel: string;
     email: string;
---
+++ src/app/usm/page.tsx
@@ -0,0 +1,46 @@
+import { Card, Flex, Heading, Table } from "@radix-ui/themes";
+import { cookies } from "next/headers";
+import { getAllUsers } from "@/app/actions/account";
+import { notFound } from "next/navigation";
+
+export default async function UsmPage() {
+    const cookieStore = await cookies();
+    const uid = cookieStore.get("uid");
+    const isSuper = false; // TODO: Check for super user
+
+    if (!isSuper) return notFound();
+
+
+    const users = await getAllUsers();
+
+    return <Card>
+        <Flex p="4" direction="column" gap="3">
+            <Heading style={{ width: "100%", textAlign: "center" }} mb="4">所有用户</Heading>
+            <Table.Root>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>UID</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>名称</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>电子邮件</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>电话</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell>身份</Table.ColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {
+                        users.map(
+                            u => <Table.Row key={u.uid}>
+                                <Table.Cell>{u.uid}</Table.Cell>
+                                <Table.Cell>{u.name}</Table.Cell>
+                                <Table.Cell>{u.email}</Table.Cell>
+                                <Table.Cell>{u.tel}</Table.Cell>
+                                <Table.Cell>{u.super ? "管理员" : "普通用户"}</Table.Cell>
+                            </Table.Row>
+                        )
+                    }
+                </Table.Body>
+            </Table.Root>
+        </Flex>
+    </Card>;
+}
